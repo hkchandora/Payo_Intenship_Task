40195f0389db0ca85b1891d875f2fc62256cf5bc
Himanshu Kumawat <chandorahimanshu@gmail.com>
2020-10-11 10:02:50 +0530

    Commit

--- app/src/main/java/com/himanshu/payo/model/Data.java
+++ app/src/main/java/com/himanshu/payo/model/Data.java
@@ -2,7 +2,6 @@ package com.himanshu.payo.model;
 
 public class Data {
 
-    int id;
     String email, first_name, last_name, avatar;
 
     public Data() {
@@ -15,44 +14,20 @@ public class Data {
         this.avatar = avatar;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getFirst_name() {
         return first_name;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
     public String getLast_name() {
         return last_name;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
     public String getAvatar() {
         return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
     }
 
 }
